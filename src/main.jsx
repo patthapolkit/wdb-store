3af89175358a9ac1aeff53c357190e9660d6c426
pawister <phaphan.phawit@gmail.com>
2024-03-12 17:06:07 +0700

    Add pages and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import Checkout from "./pages/Checkout.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import ProductList from "./pages/ProductList.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/details",
+    element: <ProductDetails />,
+  },
+  {
+    path: "/list",
+    element: <ProductList />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
