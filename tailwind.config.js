43b3ca42da693381e5298835d5ba318a05b56aba
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-03-13 16:55:42 +0700

    Add custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
-import { require } from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#FBFFE7",
+          100: "#F6FCC3",
+          300: "#E5F96E",
+          400: "#DFF547",
+          700: "#C1CD00",
+          default: "#DEF81C",
+        },
+        secondary: {
+          50: "#FAFAFA",
+          100: "#F5F5F5",
+          300: "#E1E1E1",
+          500: "#9F9F9F",
+          700: "#626262",
+          default: "#222222",
+        },
+        success: "#13CE66",
+        info: "#3366FF",
+        Warning: "#FFB020",
+        danger: "#FF000D",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
