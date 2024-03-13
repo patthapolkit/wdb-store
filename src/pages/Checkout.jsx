e1e34516b5844e87ea1579398aefa68fd825946b
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-03-13 22:40:06 +0700

    Add cart items and price summary card

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,6 +2,60 @@ import React, { Component } from "react";
 
 export default class Checkout extends Component {
   render() {
-    return <div>CHECKOUT</div>;
+    return (
+      <div className="mx-4">
+        <h1 className="font-bold text-32px my-8">My cart</h1>
+        <div className="flex flex-col gap-10">
+          <div className="bg-white p-4 flex flex-col items-center">
+            <p className="text-2xl font-bold self-start mb-6">Items</p>
+            <img
+              src="/src/assets/empty-cart.svg"
+              alt="empty-cart"
+              className="mb-6"
+            />
+            <p className="font-bold text-32px mb-2">Your cart is empty</p>
+            <p className="text-center font-semibold mb-6">
+              Looks like you have not added anything to your cart. Go ahead &
+              explore top categories.
+            </p>
+            <button className="btn rounded-none bg-secondary-base text-white font-normal text-base px-[10px] h-[54px]">
+              Continue shopping
+            </button>
+          </div>
+          <div className="bg-white p-4 flex flex-col items-center">
+            <div className="flex justify-between w-full mb-6">
+              <p className="text-2xl font-bold self-start mb-6">Summary</p>
+              <p className="font-semibold text-[18px] text-secondary-700">
+                {" "}
+                3 items
+              </p>
+            </div>
+            <div className="flex justify-between w-full pb-6 mb-6 border-b-[1px] border-secondary-300">
+              <p className="">No item</p>
+              <p className="">0.00</p>
+            </div>
+            <div className="flex justify-between w-full mb-4">
+              <p className="">Subtotal</p>
+              <p className="">0.00</p>
+            </div>
+            <div className="flex justify-between w-full mb-6 pb-6 border-b-[1px]">
+              <p className="">Shipping fee</p>
+              <p className="">Free</p>
+            </div>
+            <div className="flex justify-between w-full mb-10 font-semibold text-[18px]">
+              <p className="">Total</p>
+              <p className=" text-secondary-700">0.00</p>
+            </div>
+            <button className="btn rounded-none bg-secondary-base text-white font-normal text-base px-[10px] h-[54px] w-full mb-4">
+              Check out
+            </button>
+            <button className="btn rounded-none bg-white border font-normal text-base px-[10px] h-[54px] w-full">
+              Continue shopping
+            </button>
+          </div>
+          <h1 className="font-bold text-32px">People also like these</h1>
+        </div>
+      </div>
+    );
   }
 }
