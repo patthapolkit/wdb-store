5301b2d12752409395cdeebcfd002b3e589c3bbb
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-03-13 17:09:30 +0700

    Config daisyUI plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -30,7 +31,7 @@ export default {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     theme: false,
     base: false,
