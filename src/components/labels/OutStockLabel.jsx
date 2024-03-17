af9fa83e1a4a583960dc38535cabcc3d6bc795d3
pawister <phaphan.phawit@gmail.com>
2024-03-17 23:06:32 +0700

    Add Out of stock label component

---
+++ src/components/labels/OutStockLabel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OutStockLabel = () => {
+  return (
+    <div className="absolute right-0 mt-4 py-1.5 px-2 bg-secondary-base">
+      <p className="text-center text-xs text-white">Out of stock</p>
+    </div>
+  );
+};
+
+export default OutStockLabel;
