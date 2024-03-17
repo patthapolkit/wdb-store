8efeec3920937bf26e147824bebaecf355b9470c
pawister <phaphan.phawit@gmail.com>
2024-03-17 23:06:45 +0700

    Add sale label component

---
+++ src/components/labels/SaleLabel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SaleLabel = () => {
+  return (
+    <div className="absolute right-0 mt-4 py-1.5 px-2 bg-danger">
+      <p className="text-center text-xs text-white">Sale</p>
+    </div>
+  );
+};
+
+export default SaleLabel;
