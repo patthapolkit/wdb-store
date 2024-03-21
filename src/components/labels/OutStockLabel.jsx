0c9b798be72590d78c5b3fb3916d8e0f46f284c5
pawister <phaphan.phawit@gmail.com>
2024-03-21 23:03:58 +0700

    fix style of out of stock label

--- src/components/labels/OutStockLabel.jsx
+++ src/components/labels/OutStockLabel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const OutStockLabel = () => {
   return (
-    <div className="absolute right-0 mt-4 py-1.5 px-2 bg-secondary-base">
+    <div className="absolute right-0 mt-3.5 py-1 px-2 bg-secondary-base lg:mt-8 lg:py-2">
       <p className="text-center text-xs text-white lg:text-[27px] lg:px-4 lg:py-3">
         Out of stock
       </p>
