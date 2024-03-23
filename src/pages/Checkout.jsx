3c2aaf601a1beadbb7c563db7029e9e028f5fe1e
Nathapon <nathaponman@gmail.com>
2024-03-23 18:31:51 +0700

    test

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -8,6 +8,7 @@ export default function Checkout() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [cartId, setCartId] = useState("H8ypnablMoINttMKysN4");
+  console.log("Test");
 
   // useEffect(() => {
   //   // localStorage.setItem("cartId", "JPObn5gYjlxi6pn9MHQAb");
