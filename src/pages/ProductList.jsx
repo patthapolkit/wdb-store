d3a97d9ad3b3bae8d6f098003957b28fe5af11ac
Piengrawee Phetchuai <piengraweepechchauy@gmail.com>
2024-03-26 19:32:25 +0700

    Add product list

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,84 +1,274 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DeskNav from "../components/DeskNav";
-import Sortby from "../components/Sortby";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons"; // Importing the filter icon
 import Sidebar from "../components/Sidebar";
+import ProductCard from "../components/ProductCard";
+import { Navbar } from "../components/navbar";
 
-export default class ProductList extends Component {
-  state = {
-    menuVisible: false, // State to control the visibility of the menu items
-    bottomSheetVisible: false, // State to control the visibility of the bottom sheet
-    sortByDropdownVisible: false, // State to control the visibility of the sort by dropdown
-    isMobile: false, // State to track if the device is mobile
-    openSidebar: false,
+export default function ProductList() {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [bottomSheetVisible, setBottomSheetVisible] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [selectedOption, setSelectedOption] = useState(""); // State for selected sorting option
+  const [openSidebar, setOpenSidebar] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          "https://api.storefront.wdb.skooldio.dev/products"
+        );
+        setData(response.data.data); // Assuming data structure
+        setLoading(false);
+      } catch (error) {
+        console.error(error.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    function handleWindowSizeChange() {
+      setIsMobile(window.innerWidth < 768);
+    }
+
+    window.addEventListener("resize", handleWindowSizeChange);
+    handleWindowSizeChange();
+
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  const handleClickOutside = (e) => {
+    if (e.target.id === "bottomsheet") {
+      toggleBottomSheet();
+    }
   };
 
-  componentDidMount() {
-    // Add event listener to track window resize
-    window.addEventListener("resize", this.handleWindowSizeChange);
-    // Initialize the isMobile state based on the initial window size
-    this.handleWindowSizeChange();
-  }
+  const handleRadioChange = (value) => {
+    setSelectedOption(value);
 
-  componentWillUnmount() {
-    // Remove event listener on component unmount
-    window.removeEventListener("resize", this.handleWindowSizeChange);
-  }
+    // Implement sorting logic here
+    if (value === "lowToHigh") {
+      setData(
+        [...data].sort(
+          (a, b) =>
+            (a.promotionalPrice || a.price) - (b.promotionalPrice || b.price)
+        )
+      ); // Sort by price low to high
+    } else if (value === "highToLow") {
+      setData(
+        [...data].sort(
+          (a, b) =>
+            (b.promotionalPrice || b.price) - (a.promotionalPrice || a.price)
+        )
+      ); // Sort by price high to low
+    } else if (value === "ratings") {
+      setData([...data].sort((a, b) => b.ratings - a.ratings)); // Sort by ratings high to low (descending)
+    }
 
-  handleWindowSizeChange = () => {
-    // Update isMobile state based on window width
-    this.setState({ isMobile: window.innerWidth < 768 });
+    toggleBottomSheet(); // Close the bottom sheet
   };
 
-  // Function to toggle the visibility of the menu items
-  toggleMenu = () => {
-    this.setState({ menuVisible: !this.state.menuVisible });
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+    setOpenSidebar(!openSidebar); // Open or close the Sidebar by toggling the menu
   };
 
-  // Function to toggle the visibility of the bottom sheet
-  toggleBottomSheet = () => {
-    this.setState({ bottomSheetVisible: !this.state.bottomSheetVisible });
+  const toggleBottomSheet = () => {
+    setBottomSheetVisible(!bottomSheetVisible);
   };
 
-  render() {
-    const { isMobile, openSidebar } = this.state;
-    const gridCols = isMobile ? "grid-cols-1" : "grid-cols-3"; // Set the grid column classes based on isMobile state
-    return (
-      <div className="bg-white min-h-screen">
-        <Sidebar
-          open={openSidebar}
-          setOpen={(x) => this.setState({ openSidebar: x })}
-        />
-        <button onClick={() => this.setState({ openSidebar: true })}>
-          Open
-        </button>
-        <div className="main-content p-6">
-          <div className="topic text-center mb-4 font-bold">
-            Woman's Clothing
+  return (
+    <div className="bg-white min-h-screen">
+      <Navbar setOpenSidebar={setOpenSidebar} toggleMenu={toggleMenu} />
+      <div className="main-content p-6">
+        {/* Add flexbox layout */}
+        {!isMobile && (
+          <div className="flex">
+            <DeskNav />
+            <div className="flex">
+              <div className="name font-bold py-4">Women's Clothing</div>
+              <div className="">
+                <button
+                  className="sort-button-container items-center z-50 text-center absolute top-20 right-5 flex border border-[#C1CD00] px-2 py-2"
+                  onClick={toggleBottomSheet}
+                >
+                  Sort by
+                  <img
+                    className="items-center"
+                    src="../src/assets/chevron-down.svg"
+                    alt="Sort by"
+                  />
+                </button>
+              </div>
+              <div className="flex absolute top-20 flex-wrap gap-4 py-16">
+                {data &&
+                  data.map((product, index) => (
+                    <ProductCard
+                      key={index}
+                      image={product.imageUrls[0]}
+                      name={product.name}
+                      price={product.price}
+                      promotionalPrice={product.promotionalPrice}
+                      description={product.description}
+                      rating={product.ratings}
+                      className=""
+                    />
+                  ))}
+              </div>
+            </div>
           </div>
-          {/* Clickable "Sort By" button with filter icon on desktop */}
-          <div className="flex items-center justify-end mb-4">
-            <button
-              className="sort-button sort-button-container border
-              border-[#C1CD00] px-2 py-2"
-              onClick={this.toggleBottomSheet}
-            >
-              Sort By <FontAwesomeIcon icon={faChevronDown} />
-            </button>
+        )}
+
+        {isMobile && (
+          <>
+            <Sidebar open={openSidebar} setOpen={setOpenSidebar} />
+            <button onClick={() => setOpenSidebar(true)}>Open</button>
+            <div className="grid">
+              <div className="name font-bold py-2 text-center items-center">
+                Women's Clothing
+              </div>
+              <div className="">
+                <button
+                  className="sort-button sort-button-container text-center self-center px-1 absolute top-20 right-10 w-30 h-10"
+                  onClick={toggleBottomSheet}
+                >
+                  <div className="flex items-center">
+                    Sort by
+                    <img
+                      className="ml-2"
+                      src="../src/assets/filter.svg"
+                      alt="Sort by"
+                    />
+                  </div>
+                </button>
+              </div>
+            </div>
+            <div className="flex flex-wrap justify-between gap-4">
+              {data &&
+                data.map((product, index) => (
+                  <ProductCard
+                    key={index}
+                    image={product.imageUrls[0]}
+                    name={product.name}
+                    price={product.price}
+                    promotionalPrice={product.promotionalPrice}
+                    description={product.description}
+                    rating={product.ratings}
+                    className="w-1/3 flex flex-wrap"
+                  />
+                ))}
+            </div>
+          </>
+        )}
+
+        {bottomSheetVisible && (
+          <div
+            id="bottomsheet"
+            className={`${
+              isMobile
+                ? "bottom-sheet fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 py-4 px-8"
+                : "text-left flex justify-items-start absolute top-40 right-5"
+            }`}
+            onClick={handleClickOutside}
+          >
+            {isMobile ? (
+              <>
+                <div className="bottom-sheet-header flex justify-between items-center mb-4">
+                  <button
+                    className="cancel-button text-info font-normal"
+                    onClick={toggleBottomSheet}
+                  >
+                    Cancel
+                  </button>
+                  <h3 className="sort-by font-semibold">Sort by</h3>
+                  <button className="reset-button text-info font-normal">
+                    Reset
+                  </button>
+                </div>
+                <div className="flex flex-col">
+                  <label className="flex items-center">
+                    <input
+                      type="radio"
+                      className="mr-2"
+                      value="lowToHigh"
+                      checked={selectedOption === "lowToHigh"}
+                      onChange={() => handleRadioChange("lowToHigh")}
+                    />
+                    Price - Low to High
+                  </label>
+                  <label className="flex items-center">
+                    <input
+                      type="radio"
+                      className="mr-2"
+                      value="highToLow"
+                      checked={selectedOption === "highToLow"}
+                      onChange={() => handleRadioChange("highToLow")}
+                    />
+                    Price - High to Low
+                  </label>
+                  <label className="flex items-center">
+                    <input
+                      type="radio"
+                      className="mr-2"
+                      value="ratings"
+                      checked={selectedOption === "ratings"}
+                      onChange={() => handleRadioChange("ratings")}
+                    />
+                    Ratings
+                  </label>
+                </div>
+                <button className="bottom-sheet-footer bg-black text-white hover:bg-[#DEF81C] hover:text-black mt-4 p-2 w-full flex justify-center">
+                  <div className="apply-button flex items-center">Apply</div>
+                </button>
+              </>
+            ) : (
+              <div
+                id="bottomsheet-container"
+                className="bottom-0 right-0 w-56 rounded-md bg-white shadow-lg ring-1 ring-[#C1CD00] ring-opacity-5"
+                onClick={handleClickOutside}
+              >
+                <label className="flex items-center px-4 py-2">
+                  <input
+                    type="radio"
+                    className="mr-2"
+                    value="lowToHigh"
+                    checked={selectedOption === "lowToHigh"}
+                    onChange={() => handleRadioChange("lowToHigh")}
+                  />
+                  Price - Low to High
+                </label>
+                <label className="flex items-center px-4 py-2">
+                  <input
+                    type="radio"
+                    className="mr-2"
+                    value="highToLow"
+                    checked={selectedOption === "highToLow"}
+                    onChange={() => handleRadioChange("highToLow")}
+                  />
+                  Price - High to Low
+                </label>
+                <label className="flex items-center px-4 py-2">
+                  <input
+                    type="radio"
+                    className="mr-2"
+                    value="ratings"
+                    checked={selectedOption === "ratings"}
+                    onChange={() => handleRadioChange("ratings")}
+                  />
+                  Ratings
+                </label>
+              </div>
+            )}
           </div>
-          <div>
-            {/* Conditionally render Bottomsheet component */}
-            <Sortby
-              bottomSheetVisible={this.state.bottomSheetVisible}
-              toggleBottomSheet={this.toggleBottomSheet}
-              handleOutsideClick={this.toggleBottomSheet} // Add handleOutsideClick prop
-              isMobile={isMobile} // Pass isMobile as a prop
-            />
-          </div>
-          {!isMobile && <DeskNav />} {/* Render DeskNav only if not mobile */}
-        </div>
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
